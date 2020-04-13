cccf47d409dc2481beb84759e2fbd8bd25de2d54
JUAN-TAKO <juan.tako.dev@gmail.com>
2020-04-13 19:32:57 +0200

    grid

--- Model/Grid.java
+++ Model/Grid.java
@@ -1,5 +1,37 @@
 package Model;
 
-public class Grid {
+import java.util.ArrayList;
 
+public class Grid {
+    private ArrayList<Boolean> grid;
+    private int w, h;
+
+    public Grid(int w, int h){
+        this.w = w;
+        this.h = h;
+        this.grid = new ArrayList<Boolean>();
+        for(int i = 0; i < w*h; i++){
+            this.grid.add(true);
+        }
+    }
+
+    public ArrayList<Boolean> data(){
+        return this.grid;
+    }
+
+    public Boolean get(int x, int y){
+        return this.grid.get(x + this.w*y);
+    }
+
+    public void set(int x, int y, Boolean b){
+        this.grid.set(x + this.w*y, b);
+    }
+
+    public int width(){
+        return this.w;
+    }
+
+    public int height(){
+        return this.h;
+    }
 }
