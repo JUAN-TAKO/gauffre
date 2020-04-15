714d229d723ef442580ac19b74107c392b4801e0
juliettelempereur <juliette.lempereur@orange.fr>
2020-04-15 12:33:52 +0200

    Tests IA

--- Controller/AIPlayer.java
+++ Controller/AIPlayer.java
@@ -35,23 +35,23 @@ public class AIPlayer {
         int m=C.width();
         int n=C.height();
         ArrayList<Grid> L=new ArrayList<Grid>();
-        Grid C_suivante=new Grid(m, n); //la config que l’on renverra
+        Grid C_suivante; //la config que l’on renverra
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
                 if (C.get(i,j)==true){ /* si la case courante n’est pas encore mangée, alors on
                                                     construit une nouvelle configuration C_suivante */
-                    
+                    C_suivante=new Grid(m, n);
                     //C_suivante =C U (la matrice où le carré inférieur ou égal à C[i][j] est mangé)
                     for(int k=0; k<C.width(); k++){
                         for (int l=0; l<C.height(); l++){
-                            if ((C.get(k, l)==true)||(k<=i && l<=j)){
-                                C_suivante.set(k, l, true);
+                            if ((C.get(k, l)==false)||(k<=i && l<=j)){
+                                C_suivante.set(k, l, false);
                             }
                         }
                     }
                     
                     //ajouter C_suivante à L
-                    L.add(L.size(), C);
+                    L.add(L.size(), C_suivante);
                 }
             }
         }
