f3dd755c7f37b6d3f9e053c1fba2e995111ab51d
juliettelempereur <juliette.lempereur@orange.fr>
2020-04-15 17:33:50 +0200

    premieres méthodes Controller & HumanPlayer

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,5 +1,19 @@
-package Controller;
+import Model.Grid;
 
 public class Controller {
+    
+    public boolean finDuJeu(Grid C){ //méthode pour savoir si on a fini
+        Grid coupFinal=new Grid(C.width(), C.height());
+        for (int i=0; i<coupFinal.width(); i++){
+            for (int j=0; j<coupFinal.height(); j++){
+                coupFinal.set(i,j,false);
+            }
+        }
+        if (coupFinal==C){
+            return true;
+        }
+        
+        return false;
+    }
 
 }
--- Controller/HumanPlayer.java
+++ Controller/HumanPlayer.java
@@ -1,5 +1,26 @@
 package Controller;
+import Model.Grid;
 
 public class HumanPlayer {
+    
+    Grid configSuivante(Grid C, int i, int j){ //renvoie la config suivante en fct de là oùa appuyé le joueur,
+                                                //null si on est à la fin du jeu
+        
+        while (C.get(i,j)!=true){}; //on attend que le joueur appuie sur une case non encore mangée
+        
+        C.set(i, j, false);
+        
+        Grid C_suivante=C.copy();
+        //C_suivante =C U (la matrice où le carré inférieur ou égal à C[i][j] est mangé)
+        for(int k=0; k<C.width(); k++){
+            for (int l=0; l<C.height(); l++){
+                if (k<=i && l<=j){
+                    C_suivante.set(k, l, false);
+                }
+            }
+        }
+        return C_suivante;
+    }
+    
 
 }
